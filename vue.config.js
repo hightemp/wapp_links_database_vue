b7ed1b06690ed233e0b119b86db06e7929c21f6c
Панов Антон <hightemp.unknown@gmail.com>
2023-02-22 10:40:21 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   pwa: {
-    // iconPaths: {
-    //   favicon32: 'img/icons/favicon-32x32.png',
-    //   favicon16: 'img/icons/favicon-16x16.png',
-    //   appleTouchIcon: 'img/icons/apple-touch-icon.png',
-    //   msTileImage: 'img/icons/msapplication-icon-144x144.png'
-    // },
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon.svg',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
     manifestOptions: {
       "share_target": {
         "action": "index.html",
