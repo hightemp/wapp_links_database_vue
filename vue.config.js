2f16a13dd3f7ecd999354a6c5baed0fab60b2c43
Панов Антон <hightemp.unknown@gmail.com>
2023-02-22 10:23:20 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,13 @@ module.exports = defineConfig({
   publicPath: "./",
   outputDir: "docs",
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  }
 })
