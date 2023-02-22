d54365f1b53b2fe4744c8f7e3c0ba26b9f67e6f0
Панов Антон <hightemp.unknown@gmail.com>
2023-02-22 11:04:48 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -15,10 +15,10 @@ module.exports = defineConfig({
     // },
     iconPaths: {
       maskicon: null,
-      favicon32: "./img/icons/favicon-32x32.png",
-      favicon16: "./img/icons/favicon-16x16.png",
-      appleTouchIcon: "./img/icons/apple-touch-icon.png",
-      msTileImage: "./img/icons/msapplication-icon-144x144.png",
+      favicon32: "./favicons/favicon-32x32.png",
+      favicon16: "./favicons/favicon-16x16.png",
+      appleTouchIcon: "./favicons/apple-touch-icon-152x152.png",
+      msTileImage: "./favicons/ms-tile-144x144.png",
     },
     manifestOptions: {
       "share_target": {
@@ -36,6 +36,28 @@ module.exports = defineConfig({
           type: "image/ico",
           purpose: "any maskable",
         },
+        {
+          'src': './favicons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png'
+        },
+        {
+          'src': './favicons/android-chrome-512x512.png',
+          'sizes': '512x512',
+          'type': 'image/png'
+        },
+        {
+          'src': './favicons/android-chrome-maskable-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png',
+          'purpose': 'maskable'
+        },
+        {
+          'src': './favicons/android-chrome-maskable-512x512.png',
+          'sizes': '512x512',
+          'type': 'image/png',
+          'purpose': 'maskable'
+        }
       ],      
     }
   }
