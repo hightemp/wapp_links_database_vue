41721145b26cb315a615aef6b54b3c1b9ec9f4c0
Панов Антон <hightemp.unknown@gmail.com>
2023-02-22 10:54:14 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,20 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   pwa: {
+    // iconPaths: {
+    //   faviconSVG: 'img/icons/favicon.svg',
+    //   favicon32: 'img/icons/favicon-32x32.png',
+    //   favicon16: 'img/icons/favicon-16x16.png',
+    //   appleTouchIcon: 'img/icons/apple-touch-icon.png',
+    //   maskIcon: 'img/icons/safari-pinned-tab.svg',
+    //   msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    // },
     iconPaths: {
-      faviconSVG: 'img/icons/favicon.svg',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
-      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+      maskicon: null,
+      favicon32: "./img/icons/favicon-32x32.png",
+      favicon16: "./img/icons/favicon-16x16.png",
+      appleTouchIcon: "./img/icons/apple-touch-icon.png",
+      msTileImage: "./img/icons/msapplication-icon-144x144.png",
     },
     manifestOptions: {
       "share_target": {
@@ -21,7 +28,15 @@ module.exports = defineConfig({
           "text": "text",
           "url": "url"
         }
-      }
+      },
+      icons: [
+        {
+          src: "./favicon.ico",
+          sizes: "512x512",
+          type: "image/ico",
+          purpose: "any maskable",
+        },
+      ],      
     }
   }
 })
