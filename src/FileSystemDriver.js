cd6f9cc8610f91fd9f7633958819c93b7773d133
Панов Антон <hightemp.unknown@gmail.com>
2023-02-23 15:30:40 +0300

    update

--- src/FileSystemDriver.js
+++ src/FileSystemDriver.js
@@ -15,7 +15,6 @@ export class FileSystemDriver {
     // ===============================================================
 
     constructor (oRepoItem) {
-        console.log()
         this.fnInit(oRepoItem)
     }
 
